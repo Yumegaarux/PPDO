7251981dce5877c83971f7b886828ad32acd0cce
Yumegaarux <zaldarriagajanvictor@gmail.com>
2025-09-17 17:14:54 +0800

    login form is here no design

---
+++ view/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">>
+</head>
+<body>
+    <header>
+        <nav class="Header">
+            <div class="logo">
+                <img class="system_logo" src="images/smartpond_logo.png" alt="smartpond logo">
+                <h2>SmartPond</h2>
+            </div>
+            <div class="account">
+                <a class="alert"><img src="images/bell.png"></a>
+                <a class="change acc"><img class="dowpdown" src="images/down.png"></a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ view/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/login.css">
+</head>
+<body>
+    <div id="container">
+        <h1>Login</h1>
+        <div class="input-field">
+            <input type="text" placeholder="Username" name="user" autocomplete="off" required>
+        </div>
+        <div class="input-field">
+            <input type="password" placeholder="Password" name="pass" autocomplete="off" required>
+        </div>
+            <button class="btn">Login</button>
+        <div class="register">
+            <p>Don't have an account? <a href="">Register</a></p>
+        </div>
+    </div>
+</body>
+</html>
